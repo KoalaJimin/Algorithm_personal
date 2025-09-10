b96a1a9d9cd7aa54d7fb3d91fe80a64ddbee698b
kjm <kgmline2@naver.com>
2025-09-10 17:32:39 +0900

    [250910] - 스터디 문제 초안

---
+++ src/Swea/Swea_1868_파핑파핑지뢰찾기.java
@@ -0,0 +1,13 @@
+package Swea;
+
+import java.util.Scanner;
+
+public class Swea_1868_파핑파핑지뢰찾기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}

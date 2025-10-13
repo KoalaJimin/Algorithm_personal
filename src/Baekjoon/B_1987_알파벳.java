7f2dbd219ea2dfd48db51655401bae6d950dcef3
kjm <kgmline2@naver.com>
2025-10-13 17:47:39 +0900

    [251013] - 알파벳 해결

--- src/Baekjoon/B_1987_알파벳.java
+++ src/Baekjoon/B_1987_알파벳.java
@@ -5,65 +5,62 @@ import java.util.List;
 import java.util.Scanner;
 
 public class B_1987_알파벳 {
-	static int r, c, finishCount;
-	static char[][] arr;
-	static boolean[][] visited;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static List<Character> checkChar;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		r = sc.nextInt();
-		c = sc.nextInt();
-	
-		arr = new char[r][c];
-		visited = new boolean[r][c];
-		
-		for(int i = 0; i < r; i++) {
-			String line = sc.next();
-			for(int j = 0; j < c; j++) {
-				arr[i][j] = line.charAt(j);
-			}
-		}
-		
-		//로직 
-		checkChar = new ArrayList<>();
-		
-		for(int i = 0; i < r; i++) {
-			for(int j = 0; j < c; j++) {
-				if (!visited[i][j]) {
-					dfs(i, j);
-				}
-			}
-		}
-		
-		System.out.println(checkChar.size());
-	}
+    static int r, c, maxCount;
+    static char[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static List<Character> checkChar;
 
-	private static void dfs(int x, int y) {
-		visited[x][y] = true;
-		checkChar.add(arr[x][y]);
-	
-		for (int h = 0; h < 4; h++) {
-			int nx = x + dx[h];
-			int ny = y + dy[h];
-			
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-			if (visited[nx][ny]) continue;
-			
-			if (check(nx, ny)) dfs(nx, ny);
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        r = sc.nextInt();
+        c = sc.nextInt();
+        arr = new char[r][c];
+        visited = new boolean[r][c];
+        
+        for (int i = 0; i < r; i++) {
+            String line = sc.next();
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = line.charAt(j);
+            }
+        }
 
-	private static boolean check(int nx, int ny) {
-		for(char c : checkChar) {
-			if (c == arr[nx][ny]) return false;
-		}
-		
-		return true;
-	}
+        checkChar = new ArrayList<>();
+        dfs(0, 0, 1);
+        System.out.println(maxCount);
+    }
 
+    private static void dfs(int x, int y, int count) {
+        visited[x][y] = true;
+        checkChar.add(arr[x][y]);
+        maxCount = Math.max(maxCount, count);
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            // 범위 벗어나면 무시
+            if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
+            // 이미 방문한 좌표는 무시
+            if (visited[nx][ny]) continue;
+            // 이미 지나온 알파벳이면 무시
+            if (!check(nx, ny)) continue;
+
+            dfs(nx, ny, count + 1);
+        }
+
+        // 백트래킹 (다른 경로 탐색 위해 상태 복원)
+        visited[x][y] = false;
+        checkChar.remove(checkChar.size() - 1);
+    }
+
+    // check() 이미 지나온 알파벳인지 확인
+    private static boolean check(int nx, int ny) {
+        for (char c : checkChar) {
+            if (c == arr[nx][ny]) return false;
+        }
+        return true;
+    }
 }
